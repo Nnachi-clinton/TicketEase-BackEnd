160a17d395ceb5cb2bb1d29dad778a1142bdbec8
Idehen Ernest <135284509+Thygodfada@users.noreply.github.com>
2023-11-06 03:45:15 -0500

    PreSeeder (#6)
    * SeedingUserRoles
    
    * Correction on DbContext and Using Statements
    
    * Final Correction
    
    * Final Corrections
    
    * Final Corrections
    
    * Final Correction
    
    * Final Correction on Entities

---
+++ TicketEase.Common/Utilities/Seeder.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using TicketEase.Domain.Entities;
+
+
+namespace TicketEase.Common.Utilities
+{
+    public class Seeder
+    {
+        private static string adminRoleId;
+        private static string superAdminRoleId;
+        private static string userRoleId;
+
+        public static void SeedData(ModelBuilder builder)
+        {
+            SeedRoles(builder);
+            SeedSuperAdminUser(builder);
+        }
+
+        private static void SeedRoles(ModelBuilder builder)
+        {
+            var roles = new List<IdentityRole>
+        {
+            new IdentityRole
+            {   Id = Guid.NewGuid().ToString(),
+                Name = "Manager",
+                NormalizedName = "Manager"
+            },
+            new IdentityRole
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "SuperAdmin",
+                NormalizedName = "SuperAdmin"
+            },
+            new IdentityRole
+            {
+
+                Id = Guid.NewGuid().ToString(),
+                Name = "User",
+                NormalizedName = "User"
+            }
+        };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+
+            // Store the role IDs for later use
+            adminRoleId = roles.First(r => r.NormalizedName == "Manager").Id;
+            superAdminRoleId = roles.First(r => r.NormalizedName == "SuperAdmin").Id;
+            userRoleId = roles.First(r => r.NormalizedName == "User").Id;
+        }
+
+        private static void SeedSuperAdminUser(ModelBuilder builder)
+        {
+            var superAdminId = Guid.NewGuid().ToString();
+            var superAdminUser = new IdentityUser
+            {
+                UserName = "superadmin@library.com",
+                Email = "superadmin@library.com",
+                NormalizedEmail = "superadmin@library.com".ToUpper(),
+                NormalizedUserName = "superadmin@library.com".ToUpper(),
+                Id = superAdminId
+            };
+
+            superAdminUser.PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(superAdminUser, "SuperAdmin@123");
+            builder.Entity<IdentityUser>().HasData(superAdminUser);
+
+            // Use the stored role IDs
+            var superAdminRoles = new List<IdentityUserRole<string>>
+        {
+            new IdentityUserRole<string>
+            {
+                RoleId = adminRoleId,
+                UserId = superAdminId
+            },
+            new IdentityUserRole<string>
+            {
+                RoleId = superAdminRoleId,
+                UserId = superAdminId
+            },
+            new IdentityUserRole<string>
+            {
+                RoleId = userRoleId,
+                UserId = superAdminId
+            }
+        };
+
+            builder.Entity<IdentityUserRole<string>>().HasData(superAdminRoles);
+        }
+    }
+}
--- TicketEase.Persistence/Context/TicketEaseDbContext.cs
+++ TicketEase.Persistence/Context/TicketEaseDbContext.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TicketEase.Common.Utilities;
+using TicketEase.Domain.Entities;
 
 namespace TicketEase.Persistence.Context
 {
-    internal class TicketEaseDbContext
+    public class TicketEaseDbContext : IdentityDbContext<User>
     {
+
+        public TicketEaseDbContext(DbContextOptions<TicketEaseDbContext> options) 
+            : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Seeder.SeedData(modelBuilder);
+        }
     }
+
+   
 }
+ 
