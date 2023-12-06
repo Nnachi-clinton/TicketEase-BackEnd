7d701a521891f9c0af0f264e932bf1a95ab57270
Etinosa Idowu Omoruyi <125276315+EtinRuyi@users.noreply.github.com>
2023-12-06 12:46:27 +0100

    Bg update on manager service (#96)
    * QuickFix on AddBoard and AppUserDto
    
    * QuickFix on AddBoard/AppUserDto/UpdateUserDto
    
    * UpdatedGetAllProjects
    
    * QuickUpdateonUserDto

--- TicketEase.Application/DTO/Manager/EditManagerDto.cs
+++ TicketEase.Application/DTO/Manager/EditManagerDto.cs
@@ -7,6 +7,7 @@
         public string BusinessPhone { get; set; }
         public string CompanyAddress { get; set; }
         public string State { get; set; }
+        public string ImgUrl { get; set; }
         public DateTime UpdatedDate { get; set; }
     }
 }
